4a17a3162951f751d48007b4c3c3e943499a54db
abdelrhman <abdelrhman.zaki@ese.de>
2025-02-12 18:05:22 +0100

    d

--- test/test_func.cpp
+++ test/test_func.cpp
@@ -12,31 +12,31 @@ TEST(TEST_SUIT1, TESTCASE2)
     EXPECT_EQ(add_int(2, 2), 4);
 }
 
-TEST(TEST_SUIT1, TESTCASE3)
-{
-    SCOPED_TRACE("ss*********");
-    SCOPED_TRACE("bb----------------");
-    int x = 2;
-    int y = 3;
-    EXPECT_PRED2(int_is_eual,x, y);
+// TEST(TEST_SUIT1, TESTCASE3)
+// {
+//     SCOPED_TRACE("ss*********");
+//     SCOPED_TRACE("bb----------------");
+//     int x = 2;
+//     int y = 3;
+//     EXPECT_PRED2(int_is_eual,x, y);
 
 
-}
-TEST(TEST_SUIT1, TESTCASE4)
-{
-        int x = 2;
-    int y = 3;
-    EXPECT_TRUE(int_is_eual(x, y));
+// }
+// TEST(TEST_SUIT1, TESTCASE4)
+// {
+//         int x = 2;
+//     int y = 3;
+//     EXPECT_TRUE(int_is_eual(x, y));
 
-}
+// }
 
 
-TEST(TEST_SUIT2, TESTCASE1)
-{
-    EXPECT_EQ(add_int(2, 3), 5);
-}
+// TEST(TEST_SUIT2, TESTCASE1)
+// {
+//     EXPECT_EQ(add_int(2, 3), 5);
+// }
 
-TEST(TEST_SUIT2, TESTCASE2)
-{
-    EXPECT_EQ(add_int(2, 2), 5);
-}
+// TEST(TEST_SUIT2, TESTCASE2)
+// {
+//     EXPECT_EQ(add_int(2, 2), 5);
+// }
